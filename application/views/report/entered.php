1db79f9db815185e969cb7d204a01ead8efa415f
Robert Abon <treborlars14@gmail.com>
2022-01-11 15:54:59 +0800

    save

--- application/views/report/entered.php
+++ application/views/report/entered.php
@@ -4,61 +4,50 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/report/entered.css'); ?>">
+    <!-- <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/report/entered.css'); ?>">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <style>
         body{
             background-color: #1a1f22;
         }
 
-    </style>
+    </style> -->
     <title>Welcome</title>
         <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/Establishment/establishment_Shows.css'); ?>">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
      <style>
         body{
-            background-color: #2C272e;
+            background-color: #1a1f22;
         }
 
     </style>
 </head>
 <body>
-    <div class="container">
-        <h1>welcome</h1>
-    </div>
-    <div class="container1">
-        <a href="<?php echo site_url("tracker/display_establishment") ?>">
-            <button>Leave</button>
-        </a>
-    </div> 
     
-   <!-- <?php // print_r($report_id);
-    echo phpversion();
-   ?> -->
-    
+
     <div class="container">
-        <div class="row">
-            <div class="col">
-                <h1 class="name">Establishment Name:</h1>
-                <h1 class="location">Location:</h1>
-                <h1 class="description">Description:</h1>
-            </div>
+            <h1 class="arr2"><?php print_r($data->name)?></h1>
+            <h3 class="arr2">Welcome! We're glad that you're here</h3>
+        <div class="container1">
+            <div class="row">
                 <div class="col">
-                    <h1 class="arr2"><?php print_r($data->name)?></h1>
-                    
-                    <h4 class="arr1"><?php print_r($data->location)?></h4>
-                    <h4 class="arr1"><?php print_r($data->description)?></h4>
-
-
-
-                </div>
+                    <!-- <h1 class="name">Establishment Name:</h1> -->
+                    <div class="location1">
+                        <h1 class="location">Location:</h1>
+                        <h4 class="arr1"><?php print_r($data->location)?></h4>
+                    </div>
+                
+                    <h1 class="description">Description:</h1>
+                    <h4 class="arr3"><?php print_r($data->description)?></h4>
+                </div>      
+            </div>
         </div>
-  </div>
+    </div>
   
 <div class="wrapper">
              <a href="<?php echo site_url("tracker/display_establishment") ?>" >
-            <button>LEAVE ESTABLISHMENT</button>
+            <button class="buto1">LEAVE ESTABLISHMENT</button>
             </a>
      </div>
 
