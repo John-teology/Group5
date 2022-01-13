6e9720cf49079deafe80c016c8c53bcf6c1c89f9
Jan <76202600+John-teology@users.noreply.github.com>
2022-01-13 12:10:31 +0800

    Update db_schema.sql
    update_schema

--- deliverables/db_schema.sql
+++ deliverables/db_schema.sql
@@ -1,32 +1,10 @@
-CREATE TABLE Users (
-    UserID int,
-    Username varchar(50),
-    Pwd varchar(300),
-);
+CREATE TABLE `track`.`user` ( `id` INT NOT NULL AUTO_INCREMENT , `username` VARCHAR(50) NOT NULL , `password` VARCHAR(500) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
 
-CREATE TABLE Establishment (
-    EstablishmentID int,
-    UserID int,
-    con_tra_ID int,
-    EstablishmentName varchar(50),
-    EstablishmentLocation varchar(50),
-    EstablishmentDescription text
-);
 
-CREATE TABLE ContactTracing (
-    Con_tra_ID int,
-    UserID int,
-    EstablishmentID int,
-    first_name varchar(50)
-    last_name varchar(50)
-    email varchar(50)
-    DOB varchar(50),
+CREATE TABLE `track`.`establishment` ( `id` INT NOT NULL AUTO_INCREMENT , `userID` VARCHAR(50) NOT NULL , `location` VARCHAR(500) NOT NULL , `description` INT NOT NULL , `name` INT NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
 
-);
 
-CREATE TABLE Report (
-    ReportID int,
-    Con_Tra_ID int,
-    EstablishmentID int
-    
-);
+CREATE TABLE `track`.`contact_tracing` ( `id` INT NOT NULL AUTO_INCREMENT , `user_id` INT NOT NULL , `first_name` VARCHAR(30) NOT NULL , `last_name` VARCHAR(30) NOT NULL , `age` VARCHAR(10) NOT NULL , `email` VARCHAR(20) NOT NULL , `phone_number` VARCHAR(20) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+
+
+CREATE TABLE `track`.`reporr` ( `id` INT NOT NULL AUTO_INCREMENT , `ct_id` INT NOT NULL , `est_id` INT NOT NULL , `date_t` DATE NOT NULL DEFAULT CURRENT_TIMESTAMP , `inside` INT NULL DEFAULT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
